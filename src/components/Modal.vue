7d473168370f1a0faed5bf5b0cdf266890662623
Pablo El Maldy <pablojamiro2008@hotmail.com>
2023-09-22 02:15:45 -0500

    feat

---
+++ src/components/Modal.vue
@@ -0,0 +1,241 @@
+<script setup>
+import { computed, ref } from "vue";
+import cerrarModal from "../assets/img/cerrar.svg";
+import Alerta from "./Alerta.vue";
+
+const error = ref("");
+
+const emit = defineEmits([
+  "ocultar-modal",
+  "guardar-gasto",
+  "eliminar-gasto",
+  "update:nombre",
+  "update:cantidad",
+  "update:categoria",
+]);
+
+const props = defineProps({
+  modal: {
+    type: Object,
+    required: true,
+  },
+  nombre: {
+    type: String,
+    required: true,
+  },
+  cantidad: {
+    type: [String, Number],
+    required: true,
+  },
+  categoria: {
+    type: String,
+    required: true,
+  },
+  disponible: {
+    type: Number,
+    required: true,
+  },
+  id: {
+    type: [String, null],
+    required: true,
+  },
+});
+
+// guarda en memoria el valor inicial
+const old = props.cantidad;
+
+const agregarGasto = () => {
+  // Validar que no haya campos vacios
+  const { cantidad, categoria, nombre, id, disponible } = props;
+
+  // validar campos vacios
+  if ([nombre, categoria, cantidad].includes("")) {
+    error.value = "Todos los campos son obligatorios";
+  }
+  // Validar cantidad
+  else if (cantidad <= 0) {
+    error.value = "Cantidad no válida";
+  }
+  // en caso sea edición
+  else if (id) {
+    // tomar en cuenta el gasto antes realizado
+    if (cantidad > old + disponible) {
+      error.value = "Has excedido el presupuesto";
+    }
+  }
+  // en caso sea un registro nuevo
+  else if (cantidad > disponible) {
+    error.value = "Has excedido el presupuesto";
+  }
+
+  if (!error.value) {
+    emit("guardar-gasto");
+  }
+
+  setTimeout(() => {
+    error.value = "";
+  }, 2500);
+};
+
+const isEditing = computed(() => {
+  return props.id ? true : false;
+});
+</script>
+
+<template>
+  <div class="modal">
+    <div class="cerrar-modal">
+      <img :src="cerrarModal" @click="$emit('ocultar-modal')" />
+    </div>
+
+    <div
+      class="contenedor contenedor-formulario"
+      :class="[modal.animar ? 'animar' : 'cerrar']"
+    >
+      <form class="nuevo-gasto" @submit.prevent="agregarGasto">
+        <legend>{{ isEditing ? "Actualizar Gasto" : "Añadir Gasto" }}</legend>
+        <Alerta v-if="error">{{ error }}</Alerta>
+        <div class="campo">
+          <label for="nombre">Nombre Gasto:</label>
+          <input
+            type="text"
+            id="nombre"
+            placeholder="Añade el Nombre"
+            :value="nombre"
+            @input="($event) => $emit('update:nombre', $event.target.value)"
+          />
+        </div>
+
+        <div class="campo">
+          <label for="cantidad">Cantidad:</label>
+          <input
+            type="number"
+            id="cantidad"
+            placeholder="Añade la cantidad del Gasto, ej. 300"
+            :value="cantidad"
+            @input="($event) => $emit('update:cantidad', +$event.target.value)"
+          />
+        </div>
+
+        <div class="campo">
+          <label for="categoria">Categoría:</label>
+          <select
+            id="categoria"
+            :value="categoria"
+            @input="($event) => $emit('update:categoria', $event.target.value)"
+          >
+            <option value="">-- Seleccione --</option>
+            <option value="ahorro">Ahorro</option>
+            <option value="comida">Comida</option>
+            <option value="casa">Casa</option>
+            <option value="varios">Gastos Varios</option>
+            <option value="ocio">Ocio</option>
+            <option value="salud">Salud</option>
+            <option value="suscripciones">Suscripciones</option>
+          </select>
+        </div>
+
+        <input
+          type="submit"
+          :value="[isEditing ? 'Guardar Cambios' : 'Añadir Gasto']"
+        />
+      </form>
+      <button
+        v-if="isEditing"
+        type="button"
+        class="btn-eliminar"
+        @click="$emit('eliminar-gasto')"
+      >
+        Eliminar Gasto
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.modal {
+  position: absolute;
+  background-color: rgb(0 0 0/0.9);
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+}
+
+.cerrar-modal {
+  position: absolute;
+  top: 3rem;
+  right: 3rem;
+}
+
+.cerrar-modal img {
+  width: 3rem;
+  cursor: pointer;
+}
+
+.contenedor-formulario {
+  transition-property: all;
+  transition-duration: 300ms;
+  transition-timing-function: ease-in;
+  opacity: 0;
+}
+
+.contenedor-formulario.animar {
+  opacity: 1;
+}
+
+.contenedor-formulario.cerrar {
+  opacity: 0;
+}
+
+.nuevo-gasto {
+  margin: 10rem auto 0 auto;
+  display: grid;
+  gap: 2rem;
+}
+
+.nuevo-gasto legend {
+  text-align: center;
+  color: var(--blanco);
+  font-size: 3rem;
+  font-weight: 700;
+}
+
+.campo {
+  display: grid;
+  gap: 2rem;
+}
+
+.nuevo-gasto input,
+.nuevo-gasto select {
+  background-color: var(--gris-claro);
+  border-radius: 1rem;
+  padding: 1rem;
+  border: none;
+  font-size: 2.2rem;
+}
+
+.nuevo-gasto label {
+  color: var(--blanco);
+  font-size: 3rem;
+}
+
+.nuevo-gasto input[type="submit"] {
+  background-color: var(--azul);
+  color: var(--blanco);
+  font-weight: 700;
+  cursor: pointer;
+}
+
+.btn-eliminar {
+  border: none;
+  padding: 1.5rem;
+  width: 100%;
+  background-color: #ef4444;
+  font-weight: 700;
+  font-size: 1.2rem;
+  color: var(--blanco);
+  margin-top: 10rem;
+  cursor: pointer;
+}
+</style>
